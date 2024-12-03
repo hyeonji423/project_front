895b5c3e33a6fec604e2dd00ea7c753e4f2f25d0
Leekyoungwook <lkw001110code@gmail.com>
2024-12-03 15:40:06 +0900

    lkw1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Features from "./components/Features";
+import Home from "./components/main/Home";
+import Features from "./components/Feature";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Mypage from "./components/Mypage";
-import HealthInfo from "./components/HealthInfo";
-import NewPage from "./components/NewPage";
-import Management from "./components/Management";
-import Medidetail from "./components/Medidetail";
-import MediInfo from "./components/MediInfo";
+import Login from "./components/category/Login";
+import Register from "./components/category/Register";
+import Mypage from "./components/category/Mypage";
+import HealthInfo from "./components/category/HealthInfo";
+import NewPage from "./components/details/detailsNewPage";
+import Management from "./components/details/Management";
+import Medidetail from "./components/details/Medidetail";
+import MediInfo from "./components/category/MediInfo";
 import HealthNews from "./components/HealthNews";
-import Chatbot from "./components/Chatbot";
 import Service from "./components/Service";
 import MemberInfo from "./components/MemberInfo";
+import Chatbot from "./components/category/Chatbot";
 
 function App() {
   return (
