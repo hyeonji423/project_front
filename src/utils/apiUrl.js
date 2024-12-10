6cc7e0c89f036f41bdb781d913a949064eef9b92
ChloeLee01 <youngsunyi01@gmail.com>
2024-12-10 19:48:44 +0900

    update

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -6,6 +6,7 @@ const POST_LOGIN_API_URL = `${rootPath}/auth/login`;
 const POST_MY_MEDI_API_URL = `${rootPath}/myPage/post_myMedi`;
 const GET_MY_MEDI_LIST_API_URL = `${rootPath}/myPage/get_myMediList`;
 const DELETE_MY_MEDI_LIST_API_URL = `${rootPath}/myPage/delete_myMediList`;
+const UPDATE_MY_MEDI_LIST_API_URL = `${rootPath}/myPage/update_myMediList`;
 const GET_MEDI_INFO_API_URL = `${rootPath}/medicine/info`;
 
 export {
@@ -14,5 +15,6 @@ export {
   POST_MY_MEDI_API_URL,
   GET_MY_MEDI_LIST_API_URL,
   DELETE_MY_MEDI_LIST_API_URL,
+  UPDATE_MY_MEDI_LIST_API_URL,
   GET_MEDI_INFO_API_URL,
 };
